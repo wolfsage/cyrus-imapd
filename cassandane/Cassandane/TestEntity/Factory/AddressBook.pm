a62dd25e0fb4fdfc055c5868ebc93b51ef9fd03d
Matthew Horsfall <wolfsage@gmail.com>
2025-12-22 08:20:44 -0500

    cass Factory::AddressBook: Add 'default' method
    This retrieves the isDefault AddressBook for the user, allowing
    you to say:
    
        my $def_abook = $self->default_user->addressbooks->default;
    
    ... rather than having to query for it yourself.

--- cassandane/Cassandane/TestEntity/Factory/AddressBook.pm
+++ cassandane/Cassandane/TestEntity/Factory/AddressBook.pm
@@ -2,6 +2,7 @@ package Cassandane::TestEntity::Factory::AddressBook;
 use Moo;
 
 use feature 'state';
+use Carp ();
 
 sub fill_in_creation_defaults {
     my ($self, $prop) = @_;
@@ -10,6 +11,33 @@ sub fill_in_creation_defaults {
     $prop->{name} //= 'Address Book #' . $i++;
 
     return;
+}
+
+sub default {
+    my ($self) = @_;
+
+    my $jmap = $self->user->entity_jmap;
+    local $jmap->{CreatedIds}; # do not pollute the client for later use
+
+    my $res = $jmap->request([[ "AddressBook/get" => {} ]]);
+
+    my ($abook) = grep {
+      $_->{isDefault}
+    } $res->sentence_named("AddressBook/get")->arguments->{list}->@*;
+
+    unless ($abook && $abook->{id}) {
+      Carp::confess(
+        "Failed to find a Default AddressBook?! JMAP Resp: "
+        . explain($res)
+      );
+    }
+
+    my $id    = delete $abook->{id};
+    $self->instance_class->new({
+        id         => $id,
+        factory    => $self,
+        properties => $abook,
+    })
 }
 
 use Cassandane::TestEntity::AutoSetup;
